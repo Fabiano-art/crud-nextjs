dfb20dfd560b5bbdbe272d7703adc98bb77c91b5
Fabiano Guimarães <fabio.dub@outlook.com>
2021-09-07 16:03:21 -0300

    Initial settings

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className={'flex justify-center h-screen items-center bg-gradient-to-r from-blue-300 to-purple-900'}>
+      <span>Text</span>
+    </div>
+  )
+}
