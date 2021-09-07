a8557eacd78bb7b76c4fa2d668c41583e8b96caa
Fabiano Guimarães <fabio.dub@outlook.com>
2021-09-07 16:31:42 -0300

    Components Layout e Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
     <div className={'flex justify-center h-screen items-center bg-gradient-to-r from-blue-300 to-purple-900'}>
-      <span>Text</span>
+      <Layout titulo="Cadastro">
+        <span>Conteudo</span>
+      </Layout>
     </div>
   )
 }
